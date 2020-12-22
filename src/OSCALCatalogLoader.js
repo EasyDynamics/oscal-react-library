1a88aa263fc21597d1cf32a770bbaebb99500aea
Ray Gauss <rgauss@rgauss.com>
2020-12-22 08:16:31 -0500

    Fixed dependency warning

--- src/OSCALCatalogLoader.js
+++ src/OSCALCatalogLoader.js
@@ -61,6 +61,7 @@ export default function OSCALCatalogLoader() {
   // similar to componentDidMount()
   useEffect(() => {
     loadCatalog(catalogUrl);
+    // eslint-disable-next-line react-hooks/exhaustive-deps
   },[]);
 
   let result;
