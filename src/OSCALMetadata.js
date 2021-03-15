4f498bf3328230ebfb096e8bc4937eac58b0fb5e
kkennedy26 <78984988+kkennedy26@users.noreply.github.com>
2021-03-15 10:23:41 -0400

    Fix keys for the list items in OSCALMetadata

--- src/OSCALMetadata.js
+++ src/OSCALMetadata.js
@@ -71,9 +71,9 @@ export default function OSCALCatalogGroupControlGuidance(props) {
         	</Grid>
         	<Grid item xs={8}>
         		<Paper className={classes.OSCALMetadataParties}>
-		        	<List key="listId"
+		        	<List key={props.metadata.title + "-list"}
 		        		subheader={
-			    	        <ListSubheader className={classes.OSCALMetadataPartiesHeader} component="div" id="oscal-metadata-parties" key="header">
+			    	        <ListSubheader className={classes.OSCALMetadataPartiesHeader} component="div" id="oscal-metadata-parties" key={props.metadata.title + "-header"}>
 			    	          Parties
 			    	        </ListSubheader>
 			    	      }
