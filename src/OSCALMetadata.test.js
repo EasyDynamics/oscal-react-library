2c072a4fb8ad7544f80707a1b0633cfd469d6171
kkennedy26 <78984988+kkennedy26@users.noreply.github.com>
2021-03-16 12:07:09 -0400

    Remove unnecessary lines from Metadata file

--- src/OSCALMetadata.test.js
+++ src/OSCALMetadata.test.js
@@ -5,7 +5,7 @@ import OSCALMetadata from './OSCALMetadata';
 
 test('OSCALCatalog displays title', () => {
     const testMetadata = {title: "NIST Special Publication 800-53 Revision 5", parties:[]};
-    const testProps= {metadata: testMetadata};
+    
     render(<OSCALMetadata metadata={testMetadata} />); 
 
     const result = screen.getByText('NIST Special Publication 800-53 Revision 5');
@@ -14,7 +14,7 @@ test('OSCALCatalog displays title', () => {
 
   test('OSCALCatalog displays version', () => {
     const testMetadata = {title: "test title", parties: [], version: "Revision 5"};
-    const testProps = {metadata: testMetadata};
+    
     render(<OSCALMetadata metadata = {testMetadata} />);
   
     const result = screen.getByText("Revision 5");
