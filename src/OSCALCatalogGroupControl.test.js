e2f6cb6dd36783faa1f21070e73bb6f8c704f50f
kkennedy26 <78984988+kkennedy26@users.noreply.github.com>
2021-03-16 12:59:30 -0400

    Remove unnecessary line from OSCAL CatalogGroupControl test file

--- src/OSCALCatalogGroupControl.test.js
+++ src/OSCALCatalogGroupControl.test.js
@@ -5,9 +5,9 @@ import OSCALCatalogGroupControl from './OSCALCatalogGroupControl';
 
 test("OSCALCatalog displays controls", () => {
     const testControl = {id: "ac-1"};
-    const testProps = {control: testControl};
+    
     render(<OSCALCatalogGroupControl control = {testControl} />);
 
     const result = screen.getByText("ac-1");
-    expect(result).toHaveTextContent('ac-1');
+    expect(result).toBeVisible();
   })
