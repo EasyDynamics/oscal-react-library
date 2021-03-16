92ea730811858a2236888ce3ced4b7df7e781544
kkennedy26 <78984988+kkennedy26@users.noreply.github.com>
2021-03-16 17:12:32 -0400

    Remove empty lines in Metadata test file for consistency

--- src/OSCALMetadata.test.js
+++ src/OSCALMetadata.test.js
@@ -1,22 +1,18 @@
 import React from 'react';
-import { render ,screen } from '@testing-library/react';
+import { render, screen } from '@testing-library/react';
 import OSCALMetadata from './OSCALMetadata';
 
 
 test('OSCALCatalog displays title', () => {
     const testMetadata = {title: "NIST Special Publication 800-53 Revision 5", parties:[]};
-    
     render(<OSCALMetadata metadata={testMetadata} />); 
-
     const result = screen.getByText('NIST Special Publication 800-53 Revision 5');
     expect(result).toBeVisible();
   })
 
 test('OSCALCatalog displays version', () => {
     const testMetadata = {title: "test title", parties: [], version: "Revision 5"};
-    
     render(<OSCALMetadata metadata = {testMetadata} />);
-  
     const result = screen.getByText("Revision 5");
     expect(result).toBeVisible();
   })
