59f478205c727e7b5cf3a427411fafd5c097b73b
Ray Gauss II <rgauss@easydynamics.com>
2021-03-17 13:24:16 -0400

    Fix import spacing
    Co-authored-by: Hannah Posey-Scholl <hkps.hannah@gmail.com>

--- src/OSCALControlImplementationImplReq.test.js
+++ src/OSCALControlImplementationImplReq.test.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { render ,screen} from '@testing-library/react';
+import { render, screen } from '@testing-library/react';
 import OSCALControlImplementation from './OSCALControlImplementation.js';
 
 const controlImplData = {
