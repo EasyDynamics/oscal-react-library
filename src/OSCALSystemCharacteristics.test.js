a239e3eb1af9a93aa55afdace7fe51736646d31d
Ray Gauss II <rgauss@easydynamics.com>
2021-03-17 13:27:55 -0400

    Fix import spacing
    Co-authored-by: Hannah Posey-Scholl <hkps.hannah@gmail.com>

--- src/OSCALSystemCharacteristics.test.js
+++ src/OSCALSystemCharacteristics.test.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { render ,screen } from '@testing-library/react';
+import { render, screen } from '@testing-library/react';
 import OSCALSystemCharacteristics from './OSCALSystemCharacteristics.js';
 
 export const systemCharacteristicsTestData = {
