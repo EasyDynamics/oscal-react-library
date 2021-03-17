a3daa0de37efbc4a668b266e13b70d20c6d7522f
kkennedy26 <78984988+kkennedy26@users.noreply.github.com>
2021-03-17 09:11:57 -0400

    Remove empty lines and spacing within CatalogGroupControlGuidance test file

--- src/OSCALCatalogGroupControlGuidance.test.js
+++ src/OSCALCatalogGroupControlGuidance.test.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { render ,screen} from '@testing-library/react';
+import { render, screen } from '@testing-library/react';
 import OSCALCatalogGroupControlGuidance from './OSCALCatalogGroupControlGuidance';
 import userEvent from '@testing-library/user-event';
 
@@ -7,10 +7,7 @@ import userEvent from '@testing-library/user-event';
 test("OSCALCatalog displays control guidance", ()=>{
     const prose ="Access control policy"
     render(<OSCALCatalogGroupControlGuidance prose = {prose} />) 
-  
-    //create an event for the button 
     userEvent.click(screen.getByRole('button'));
-
     const result = screen.getByText("Access control policy");
     expect(result).toBeVisible();
   })
