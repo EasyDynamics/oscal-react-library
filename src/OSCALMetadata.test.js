3393351f0da32e8b94267a86ed550070f8ad7002
kkennedy26 <78984988+kkennedy26@users.noreply.github.com>
2021-03-17 12:53:33 -0400

    Fix quotation marks in Metadata test file for consistency

--- src/OSCALMetadata.test.js
+++ src/OSCALMetadata.test.js
@@ -6,7 +6,7 @@ import OSCALMetadata from './OSCALMetadata';
 test('OSCALCatalog displays title', () => {
     const testMetadata = {title: "NIST Special Publication 800-53 Revision 5", parties:[]};
     render(<OSCALMetadata metadata={testMetadata} />); 
-    const result = screen.getByText('NIST Special Publication 800-53 Revision 5');
+    const result = screen.getByText("NIST Special Publication 800-53 Revision 5");
     expect(result).toBeVisible();
   })
 
