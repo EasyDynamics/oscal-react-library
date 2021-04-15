b9627530d0f379984132ceedb594699f51a90eb2
kkennedy26 <78984988+kkennedy26@users.noreply.github.com>
2021-04-15 10:29:57 -0400

    Fix inconsistent indentation in OSCALComponentDefinition.js

--- src/OSCALComponentDefinition.js
+++ src/OSCALComponentDefinition.js
@@ -31,50 +31,50 @@ const useStyles = makeStyles((theme) => ({
   export default function OSCALComponentDefinition(props) {
 	const classes = useStyles();
 	    
-        return (
-	        <div className={classes.paper}>
-	            <OSCALMetadata metadata={props.componentDefinition.metadata} />
+		return (
+			<div className={classes.paper}>
+				<OSCALMetadata metadata={props.componentDefinition.metadata} />
 				<div className={classes.paper}>
 					<Card>
-	    		    	<CardContent>
-	    		    	<Grid container spacing={2}>
-		    		    	<Grid item xs={12} className={classes.OSCALComponentsHeader}>
-			        	    	<Typography>
-			        		    	Components
-				            	</Typography>
-		        	    	</Grid>
+						<CardContent>
+						<Grid container spacing={2}>
+							<Grid item xs={12} className={classes.OSCALComponentsHeader}>
+								<Typography>
+									Components
+								</Typography>
+							</Grid>
 							<Grid item xs={12}>
-			            		<TableContainer>
-				            		<Table aria-label="Title">
+								<TableContainer>
+									<Table aria-label="Title">
 										<TableHead>
-				                			<TableRow>
-				                    			<TableCell>Title</TableCell>
-				                    			<TableCell>Description</TableCell>
-				                    			<TableCell>Responsible Roles</TableCell>
-				                			</TableRow>
-				              		  	</TableHead>
-				              			<TableBody>
-                                			{Object.entries(props.componentDefinition.components).map(([key, component], index) => (
+											<TableRow>
+												<TableCell>Title</TableCell>
+												<TableCell>Description</TableCell>
+												<TableCell>Responsible Roles</TableCell>
+											</TableRow>
+										</TableHead>
+										<TableBody>
+											{Object.entries(props.componentDefinition.components).map(([key, component], index) => (
 											<TableRow key={key}>
-				                    			<TableCell component="th" scope="row">
-				                    				<Tooltip title={component.description}>
-				                    					<Typography variant="body2">{component.title}</Typography>
-				                    				</Tooltip>
-				                    			</TableCell>
-				                    			<TableCell>{component.type}</TableCell>
-				                    			<TableCell>
+												<TableCell component="th" scope="row">
+													<Tooltip title={component.description}>
+														<Typography variant="body2">{component.title}</Typography>
+													</Tooltip>
+												</TableCell>
+												<TableCell>{component.type}</TableCell>
+												<TableCell>
 													<OSCALResponsibleRoles responsibleRoles={component.['responsible-roles']} parties={props.componentDefinition.metadata.parties} />
-				                    			</TableCell>
-				                  			</TableRow>
-				                			))}
-				              			</TableBody>
-				            		</Table>
-				        		</TableContainer>
-		        			</Grid>
+												</TableCell>
+											</TableRow>
+											))}
+										</TableBody>
+									</Table>
+								</TableContainer>
+							</Grid>
 						</Grid>
 						</CardContent>
 					</Card>
 				</div>
-	        </div>
-	    );
+			</div>
+		);
 	}
