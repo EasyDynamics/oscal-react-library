c7991ef0164504293cd821c3be0e21fdf4bc7816
Ray Gauss <rgauss@rgauss.com>
2021-04-28 07:49:02 -0400

    Updatade OSCALComponentResolver to use new recursive method

--- src/oscal-utils/OSCALComponentResolver.js
+++ src/oscal-utils/OSCALComponentResolver.js
@@ -1,15 +1,25 @@
-import ResolveProfileOrCatalogUrl from "./OSCALProfileResolver";
+import OSCALResolveProfileOrCatalogUrlControls from "./OSCALProfileResolver";
 
+/* eslint no-param-reassign: ["error", { "props": true, "ignorePropertyModificationsFor": ["componentDefinition"] }] */
 export default function OSCALComponentResolveSources(
     componentDefinition,
     parentUrl,
     onSuccess,
     onError
   ) {
-  Object.entries(componentDefinition.components).forEach(([key, component]) => {
-    let controlImplementation;
-    component.controlImplementations.forEach(controlImplementation => {
-      ResolveProfileOrCatalogUrl(componentDefinition.controls, controlImplementation.source, parentUrl, onSuccess, onError);
+    componentDefinition.resolvedControls = [];
+    Object.entries(componentDefinition.components).forEach(([key, component]) => {
+      let controlImplementation;
+      component.controlImplementations.forEach(controlImplementation => {
+        OSCALResolveProfileOrCatalogUrlControls(
+          componentDefinition.resolvedControls,
+          controlImplementation.source,
+          parentUrl,
+          componentDefinition.["back-mattter"], // not actually used for resolution in components
+          onSuccess,
+          onError,
+          []
+        );
+      });
     });
-  });
 }
