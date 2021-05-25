51378e17d125d4bae3d7047c852586e4da4164bb
kkennedy26 <78984988+kkennedy26@users.noreply.github.com>
2021-05-25 08:52:04 -0400

    Add initial implementation of profile tests, metadata test

---
+++ src/OSCALProfile.test.js
@@ -0,0 +1,38 @@
+import React from "react";
+import { render, screen } from "@testing-library/react";
+import { OSCALProfileLoader } from "./OSCALLoader";
+import OSCALProfile from "./OSCALProfile";
+import { metadataTestData, testOSCALMetadata } from "./OSCALMetadata.test";
+import { controlsData } from "./OSCALComponentDefinitionControlImplementation.test";
+
+const profileTestData = {
+  uuid: "3afae418-b105-47ba-b51d-653a1a6b9267",
+  metadata: metadataTestData,
+  imports: [
+    {
+      href: "#dc380596-027f-423b-83f2-82757554ee27",
+      "include-controls": [
+        {
+          "with-ids": [
+              "control-1",
+              "control-1.1",
+              "control-2",
+              "control-2.1"
+          ],
+        },
+      ],
+    },
+  ],
+  resolvedControls: controlsData,
+  trimmedControls: ["control-1", "control-1.1", "control-2", "control-2.1"],
+};
+
+test("OSCALProfile loads", () => {
+  render(<OSCALProfileLoader />);
+});
+
+function profileRenderer() {
+  render(<OSCALProfile profile={profileTestData} />);
+}
+
+testOSCALMetadata("OSCALProfile", profileRenderer);
