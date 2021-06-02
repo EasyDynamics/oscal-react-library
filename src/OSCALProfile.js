d8e6dfa83fcab9451fb1e0c61a121ba8dbc60b4b
Kyle Kennedy <kkennedy@easydynamics.com>
2021-06-02 14:10:47 -0400

    Add forEach() method to loop through nested array and pull out 'with-ids' in Profile

--- src/OSCALProfile.js
+++ src/OSCALProfile.js
@@ -2,14 +2,6 @@ import React, { useState, useEffect } from "react";
 import { makeStyles } from "@material-ui/core/styles";
 import List from "@material-ui/core/List";
 import ListSubheader from "@material-ui/core/ListSubheader";
-import Table from "@material-ui/core/Table";
-import TableBody from "@material-ui/core/TableBody";
-import TableCell from "@material-ui/core/TableCell";
-import TableContainer from "@material-ui/core/TableContainer";
-import TableHead from "@material-ui/core/TableHead";
-import TableRow from "@material-ui/core/TableRow";
-import Tooltip from "@material-ui/core/Tooltip";
-import Typography from "@material-ui/core/Typography";
 import OSCALMetadata from "./OSCALMetadata";
 import OSCALProfileDefintionResolver from "./oscal-utils/OSCALProfileDefinitionResolver";
 import OSCALControl from "./OSCALControl";
@@ -46,9 +38,11 @@ export default function OSCALProfile(props) {
     // eslint-disable-next-line
   }, []);
 
-  // TODO - this is a bad way to grab imported controls, but due to nested arrays everywhere it is somewhat necessary
-  profile.includeControlIds =
-    profile.imports[0]["include-controls"][0]["with-ids"];
+  profile.imports.forEach((imp) => {
+    imp["include-controls"].forEach((includeControl) => {
+      profile.includeControlIds = includeControl["with-ids"];
+    });
+  });
 
   let profileImports;
 
