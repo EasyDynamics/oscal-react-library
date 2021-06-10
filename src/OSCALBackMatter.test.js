9960052fc0cc4989af73a34a46db41ea4aba7812
Kyle Kennedy <kkennedy@easydynamics.com>
2021-06-10 15:35:59 -0400

    Add test file for Back Matter Component called OSCALBackMatter.test.js

---
+++ src/OSCALBackMatter.test.js
@@ -0,0 +1,43 @@
+import React from "react";
+import { render, screen } from "@testing-library/react";
+import userEvent from "@testing-library/user-event";
+import OSCALBackMatter from "./OSCALBackMatter";
+
+const backMatterTestData = {
+  resources: [
+    {
+      uuid: "dc380596-027f-423b-83f2-82757554ee27",
+      description: "This is a test description for resource",
+      rlinks: [
+        {
+          href: "NIST_SP-800-53_rev4_catalog.xml",
+          "media-type": "application/oscal.catalog+json"
+        }
+      ],
+    },
+  ],
+}
+
+function backMatterRenderer() {
+  render(<OSCALBackMatter backMatter={backMatterTestData} />);
+}
+
+export function testOSCALBackMatter(parentElementName, renderer) {
+  test(`${parentElementName} displays title`, () => {
+    renderer();
+    const result = screen.getByText("No Title");
+    expect(result).toBeVisible();
+  });
+
+  test(`${parentElementName} displays resource description`, async () => {
+    renderer();
+    userEvent.hover(screen.getByText("No Title"));
+    expect(
+      await screen.findByText("This is a test description for resource")
+    ).toBeInTheDocument();
+  });
+}
+
+if (!require.main) {
+  testOSCALBackMatter("OSCALBackMatter", backMatterRenderer);
+}
