b1188d94fc4f71137fb2fa9918f3f2b4c81ed534
Zachary Tucker <ztucker@easydynamics.com>
2021-06-14 13:42:43 -0400

    Handle different control modification definitions
    FedRAMP & NIST are defining control modifications in slightly different
    ways. This on top of even more changes to FedRAMP's definition of a
    control modification caused OSCALProfiles to load incorrectly. We now
    deal with those changes in a cleaner and less repetitive way. This
    should make dealing with changes in the future easier as well.

--- src/OSCALControlModification.js
+++ src/OSCALControlModification.js
@@ -17,6 +17,100 @@ const useStyles = makeStyles((theme) => ({
   },
 }));
 
+/**
+ * Create a typography html object for Add/Remove DialogContent
+ *
+ * @param {*} element add or remove object to map into html
+ * @param {*} funcText html text to display
+ * @returns html object
+ */
+const getTypography = (element, funcText) => {
+  let typograhy;
+  if (element.props) {
+    typograhy = element.props.map((prop) => (
+      <Typography color="textsecondary" paragraph="true" variant="body1">
+        Name:{prop.name}, Value:{prop.value}
+      </Typography>
+    ));
+  } else if (element.parts) {
+    typograhy = element.parts.map((prop) => (
+      <Typography color="textsecondary" paragraph="true" variant="body1">
+        Name:{prop.name}, Value:{prop.value}
+      </Typography>
+    ));
+  }
+  return (
+    // TODO - consider making this into a table
+    <DialogContentText
+      color="textprimary"
+      id="scroll-dialog-description"
+      tabIndex={-1}
+      variant="h6"
+    >
+      {funcText}
+      {typograhy}
+    </DialogContentText>
+  );
+};
+
+/**
+ * TODO: Add/Remove ids do not exactly equal controlId's
+ * checkID attempts to match relevant add/remove object ids
+ * with the controlID. This only applies to FedRAMP profiles such
+ * as: https://raw.githubusercontent.com/GSA/fedramp-automation/master/baselines/rev4/json/FedRAMP_rev4_MODERATE-baseline_profile.json
+ *
+ * @param {*} controlId Control ID to match
+ * @param {*} element Add/Remove element to check id of
+ * @param {*} field Field of Add/Remove element to check
+ * @returns true if element matches controlID
+ */
+const checkID = (controlId, element, field) => {
+  let equal = false;
+
+  // TODO: Differences in how NIST and FedRAMP implement adds makes
+  // this check needed. See if we can clean this up at some point.
+  // Assumes the difference is that NIST does not have a "control-id" field
+  if (!element[field]) return true;
+
+  // Throw out invalid id's
+  if (element[field].includes("obj")) return false;
+
+  // Check if id matches
+  if (element[field].includes(controlId)) equal = true;
+  else if (element[field] === controlId.split("_")[0]) equal = true;
+
+  return equal;
+};
+
+/**
+ * Get the modifications from the adds/removes list.
+ *
+ * @param {*} controlId Control Id to check compare element id's with
+ * @param {*} modList List of modifications
+ * @param {*} modText String to display type of modification
+ * @returns an HTML element
+ */
+const getModifcations = (controlId, modList, modText) => {
+  let modLength = 0;
+  const controlParts = [];
+
+  // Add everything with ids that match controlPartId
+  modList.forEach((element) => {
+    if (checkID(controlId, element, "by-id")) {
+      modLength += 1;
+      controlParts.push(element);
+    }
+  });
+
+  // return display & mod length
+  return [
+    <DialogContent dividers>
+      {controlParts.map((element) => getTypography(element, modText))}
+    </DialogContent>,
+    modLength,
+  ];
+};
+
 export default function OSCALControlModification(props) {
   if (!props.modifications || !props.modifications.alters) {
     return null;
@@ -33,117 +127,44 @@ export default function OSCALControlModification(props) {
     setOpen(false);
   };
 
-  // Check the number of adds and removes for a number to be displayed as the badge
-  const getModLength = (adds, removes) => {
-    let addsLength = 0;
-    let removesLength = 0;
-    if (adds) {
-      addsLength = adds.length;
-    }
-    if (removes) {
-      removesLength = removes.length;
-    }
-    return addsLength + removesLength;
-  };
-
   // Finds the control-id within alters and matches it with a resolved control
   const alter = props.modifications.alters.find(
     (element) => element["control-id"] === props.control.id
   );
 
   let modificationsDisplay;
-  let addObject;
-  let removeObject;
   // eslint-disable-next-line
-  const addRemoveControlPart = [];
-  let addsDisplay;
-  let removesDisplay;
+  const addControlPart = [];
+  const removeControlPart = [];
+  let addsDisplay = null;
+  let removesDisplay = null;
+  let len;
+  let modLength = 0;
 
   if (alter) {
+    // Get all add modifications
     if (alter.adds) {
-      // iterate through each "add" and check if the id-ref matches props.partId
-      // if it matches, push the "add" to the array
-      alter.adds.forEach((add) => {
-        if (add["id-ref"]) {
-          addObject = add.find((add) => add["id-ref"] === props.controlPartId);
-          addRemoveControlPart.push(...addObject);
-        }
-      });
-      addsDisplay = (
-        <DialogContent dividers>
-          {alter.adds.map((add) => (
-            // TODO - consider making this into a table
-            <DialogContentText
-              color="textprimary"
-              id="scroll-dialog-description"
-              tabIndex={-1}
-              variant="h6"
-            >
-              Adds:
-              {add.props.map((prop) => (
-                <Typography
-                  color="textsecondary"
-                  paragraph="true"
-                  variant="body1"
-                >
-                  Name:{prop.name}, Value:{prop.value}
-                </Typography>
-              ))}
-            </DialogContentText>
-          ))}
-        </DialogContent>
+      [addsDisplay, len] = getModifcations(
+        props.controlPartId,
+        alter.adds,
+        "Adds:"
       );
+      modLength += len;
     }
 
+    // Get all remove modifications
     if (alter.removes) {
-      // iterate through each "add" and check if the id-ref matches props.partId
-      // if it matches, push the "add" to the array
-      alter.removes.forEach((remove) => {
-        if (remove["id-ref"]) {
-          removeObject = remove.find(
-            (remove) => remove["id-ref"] === props.controlPartId
-          );
-          addRemoveControlPart.push(...removeObject);
-        }
-      });
-      removesDisplay = (
-        <DialogContent dividers>
-          {alter.removes.map((remove) => (
-            // TODO - consider making this into a table
-            <DialogContentText
-              color="textprimary"
-              id="scroll-dialog-description"
-              tabIndex={-1}
-              variant="h6"
-            >
-              Removes:
-              <Typography
-                color="textsecondary"
-                paragraph="true"
-                variant="body1"
-              >
-                id-ref: {remove["id-ref"]}
-                name-ref: {remove["name-ref"]}
-              </Typography>
-            </DialogContentText>
-          ))}
-        </DialogContent>
+      [removesDisplay, len] = getModifcations(
+        props.controlPartId,
+        alter.removes,
+        "Removes:"
       );
+      modLength += len;
     }
   }
 
-  // For each addRemoveControlPart find the add or remove that matches the part id
-  let controlPartObject;
-  if (props.partId) {
-    addRemoveControlPart.forEach((addRemove) => {
-      controlPartObject = addRemove.find(
-        (addRemove) => addRemove["id-ref"] === props.controlPartId
-      );
-    });
-  }
-
-  // Display if alter or controlPartObject is true
-  if (alter || controlPartObject) {
+  // Display if altered is true
+  if (alter) {
     modificationsDisplay = (
       <div>
         <Tooltip title="Modifications">
@@ -153,7 +174,7 @@ export default function OSCALControlModification(props) {
               horizontal: "right",
             }}
             color="secondary"
-            badgeContent={getModLength(alter.adds, alter.removes)}
+            badgeContent={modLength}
             overlap="circle"
           >
             <IconButton
