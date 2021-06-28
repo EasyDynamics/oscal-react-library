0320bf95db9100a170027baa3b725c248b990524
Zachary Tucker <ztucker@easydynamics.com>
2021-06-28 10:46:30 -0400

    Start linting all .test.js files

--- src/components/OSCALControlProse.test.js
+++ src/components/OSCALControlProse.test.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { render, screen, fireEvent, act } from "@testing-library/react";
+import { render, screen, fireEvent } from "@testing-library/react";
 import { OSCALReplacedProseWithParameterLabel } from "./OSCALControlProse";
 
 const labelTestData = "label-1";
