39b895773c3cd165b759756fbcd7ff1063215482
Bronstrom <bfellstrom@easydynamics.com>
2021-07-09 17:06:32 -0400

    Add initial Material-UI Skeleton component
    During the page loading process in Profile Viewer, a Skeleton
    placeholder is added as a visual indicator. The added placeholder is
    modeled after the card structure of the imported controls. Additional
    documentation has been included in Profile as well.

--- src/components/OSCALProfile.js
+++ src/components/OSCALProfile.js
@@ -2,6 +2,8 @@ import React, { useState, useEffect, useRef } from "react";
 import { makeStyles } from "@material-ui/core/styles";
 import List from "@material-ui/core/List";
 import ListSubheader from "@material-ui/core/ListSubheader";
+import Skeleton from "@material-ui/lab/Skeleton";
+import CardContent from "@material-ui/core/CardContent";
 import OSCALMetadata from "./OSCALMetadata";
 import OSCALControl from "./OSCALControl";
 import OSCALBackMatter from "./OSCALBackMatter";
@@ -15,12 +17,21 @@ const useStyles = makeStyles((theme) => ({
   },
 }));
 
+/**
+ * Displays a given OSCAL Profile is an easily consumable format. According to NIST, a profile
+ * represents the baseline of selected controls from one or more control catalogs.
+ * For more information see: https://pages.nist.gov/OSCAL/concepts/layer/control/profile/
+ *
+ * @param {Object} props
+ * @returns The OSCAL profile component
+ */
 export default function OSCALProfile(props) {
   const [error, setError] = useState(null);
   const [isLoaded, setIsLoaded] = useState(false);
   const classes = useStyles();
   const unmounted = useRef(false);
 
+  // Using oscal-utils resolves the profile, provides are error when failure
   useEffect(() => {
     OSCALResolveProfile(
       props.profile,
@@ -43,42 +54,52 @@ export default function OSCALProfile(props) {
     };
   }, []);
 
+  // Flatten controls and IDs into single array
   const includeControlIds = props.profile.imports
     .flatMap((imp) => imp["include-controls"])
     .flatMap((includeControl) => includeControl["with-ids"]);
 
-  let profileImports;
+  // Imports resolved controls when loaded. When loading, displays a basic skeleton placeholder
+  // of the content.
+  const profileImports = (
+    <List
+      className={classes.OSCALControlList}
+      subheader={
+        <ListSubheader
+          className={classes.OSCALMetadataPartiesHeader}
+          component="div"
+          id="oscal-profile-importedControls"
+          disableSticky
+        >
+          Imported Controls
+        </ListSubheader>
+      }
+    >
+      {!isLoaded
+        ? [...Array(3)].map(() => (
+            <CardContent>
+              <span style={{ marginTop: 5, display: "flex", gap: "1em" }}>
+                <Skeleton variant="text" width="25em" height="3em" />
+                <Skeleton variant="circle" width="3em" height="3em" />
+              </span>
+              <Skeleton variant="text" width="10em" height="2.5em" />
+              <Skeleton variant="rect" width="100%" height={115} />
+              <Skeleton variant="text" width="6.5em" height="3.5em" />
+            </CardContent>
+          ))
+        : props.profile.resolvedControls.map((control) => (
+            <OSCALControl
+              control={control}
+              includeControlIds={includeControlIds}
+              modifications={props.profile.modify}
+              childLevel={0}
+              key={`control-${control.id}`}
+            />
+          ))}
+    </List>
+  );
 
-  if (!isLoaded) {
-    profileImports = null;
-  } else {
-    profileImports = (
-      <List
-        className={classes.OSCALControlList}
-        subheader={
-          <ListSubheader
-            className={classes.OSCALMetadataPartiesHeader}
-            component="div"
-            id="oscal-profile-importedControls"
-            disableSticky
-          >
-            Imported Controls
-          </ListSubheader>
-        }
-      >
-        {props.profile.resolvedControls.map((control) => (
-          <OSCALControl
-            control={control}
-            includeControlIds={includeControlIds}
-            modifications={props.profile.modify}
-            childLevel={0}
-            key={`control-${control.id}`}
-          />
-        ))}
-      </List>
-    );
-  }
-
+  // Display Metadata and BackMatter components at bottom of Profile
   return (
     <div className={classes.paper}>
       <OSCALMetadata metadata={props.profile.metadata} />
