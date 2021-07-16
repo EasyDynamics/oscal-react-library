080b74cd121bed92bfa9df8b87d8277002d0bf8c
Sepideh Norouzzadeh <snorouzzadeh@easydynamics.com>
2021-07-16 09:18:42 -0400

    Add variations to Back Matter Component stories
    Implemented an incremental approach by adding
    optional props/complexity to each variation.
    
    Added the display of title, description,
    citation, and media type to the .stories.js
    file for the Back Matter Component.

--- src/stories/OSCALBackMatter.stories.js
+++ src/stories/OSCALBackMatter.stories.js
@@ -10,10 +10,87 @@ const Template = (args) => <OSCALBackMatter {...args} />;
 
 export const Default = Template.bind({});
 
+export const BackMatterTitle = Template.bind({});
+
+export const BackMatterDescription = Template.bind({});
+
+export const BackMatterCitation = Template.bind({});
+
+export const BackMatterMediaType = Template.bind({});
+
 const exampleBackMatter = {
   resources: [
     {
       uuid: "dc380596-027f-423b-83f2-82757554ee27",
+      description:
+        "NIST Special Publication 800-53 Revision 4: Security and Privacy Controls for Federal Information Systems and Organizations",
+      rlinks: [
+        {
+          href: "NIST_SP-800-53_rev4_catalog.json",
+          "media-type": "application/oscal.catalog+json",
+        },
+      ],
+    },
+  ],
+};
+
+const exampleBackMatterTitle = {
+  resources: [
+    {
+      uuid: "dc380596-027f-423b-83f2-82757554ee27",
+      title: "Resource Title",
+      rlinks: [
+        {
+          href: "NIST_SP-800-53_rev4_catalog.json",
+        },
+      ],
+    },
+  ],
+};
+
+const exampleBackMatterDescription = {
+  resources: [
+    {
+      uuid: "dc380596-027f-423b-83f2-82757554ee27",
+      title: "Resource Title",
+      description:
+        "NIST Special Publication 800-53 Revision 4: Security and Privacy Controls for Federal Information Systems and Organizations",
+      rlinks: [
+        {
+          href: "NIST_SP-800-53_rev4_catalog.json",
+        },
+      ],
+    },
+  ],
+};
+
+const exampleBackMatterCitation = {
+  resources: [
+    {
+      uuid: "dc380596-027f-423b-83f2-82757554ee27",
+      title: "Resource Title",
+      citation: {
+        text: "This is an example citation",
+      },
+      description:
+        "NIST Special Publication 800-53 Revision 4: Security and Privacy Controls for Federal Information Systems and Organizations",
+      rlinks: [
+        {
+          href: "NIST_SP-800-53_rev4_catalog.json",
+        },
+      ],
+    },
+  ],
+};
+
+const exampleBackMatterMediaType = {
+  resources: [
+    {
+      uuid: "dc380596-027f-423b-83f2-82757554ee27",
+      title: "Resource Title",
+      citation: {
+        text: "This is an example citation",
+      },
       description:
         "NIST Special Publication 800-53 Revision 4: Security and Privacy Controls for Federal Information Systems and Organizations",
       rlinks: [
@@ -31,5 +108,25 @@ const exampleParentUrl =
 
 Default.args = {
   backMatter: exampleBackMatter,
+  parentUrl: exampleParentUrl,
+};
+
+BackMatterTitle.args = {
+  backMatter: exampleBackMatterTitle,
+  parentUrl: exampleParentUrl,
+};
+
+BackMatterDescription.args = {
+  backMatter: exampleBackMatterDescription,
+  parentUrl: exampleParentUrl,
+};
+
+BackMatterCitation.args = {
+  backMatter: exampleBackMatterCitation,
+  parentUrl: exampleParentUrl,
+};
+
+BackMatterMediaType.args = {
+  backMatter: exampleBackMatterMediaType,
   parentUrl: exampleParentUrl,
 };
