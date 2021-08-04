6dd1ac43359d509a1ad62dcbaab720d817baff7b
hreineck <hreineck@easydynamics.com>
2021-08-04 17:44:42 -0400

    Fix Modification Rendering Issues
    Change the way the `isLoaded` state variable is set to
    fix an issue where modifications would not render in the
    Component Viewer.
    `isLoaded` was being set to true upon the modifications for the
    first Control Implementation being resolved, which was causing
    the state to be "loaded" when not all the modifications were loaded.
    Added a `pendingProcesses` counter to track when all of the
    implementations have had their modifications resolved before
    setting the isLoaded state.

--- src/components/oscal-utils/OSCALComponentResolver.js
+++ src/components/oscal-utils/OSCALComponentResolver.js
@@ -1,5 +1,13 @@
 import OSCALResolveProfileOrCatalogUrlControls from "./OSCALProfileResolver";
 
+function processesToRun(componentDefinition) {
+  let count = 0;
+  componentDefinition.components.forEach((component) => {
+    count += component["control-implementations"]?.length ?? 0;
+  });
+  return count;
+}
+
 export default function OSCALComponentResolveSources(
   componentDefinition,
   parentUrl,
@@ -9,6 +17,7 @@ export default function OSCALComponentResolveSources(
   // Fixing linting error here would take significant change to codebase given how we use props.
   /* eslint no-param-reassign: "error" */
   componentDefinition.resolvedControls = [];
+  let pendingProcesses = processesToRun(componentDefinition);
   Object.entries(componentDefinition.components).forEach(([key, component]) => {
     component.uuid = key;
     component["control-implementations"].forEach((controlImplementation) => {
@@ -23,8 +32,16 @@ export default function OSCALComponentResolveSources(
         controlImplementation.source,
         parentUrl,
         componentDefinition["back-matter"], // not actually used for resolution in components
-        onSuccess,
-        onError,
+        () => {
+          pendingProcesses--;
+          if (pendingProcesses == 0) {
+            onSuccess();
+          }
+        },
+        () => {
+          pendingProcesses--;
+          onError();
+        },
         []
       );
     });
