de713117a650d3460544fed9fe68625bbddba25d
Michael Eisenberg <meisenberg@easydynamics.com>
2021-11-12 10:58:45 -0500

    Remove unused imports

--- src/components/OSCALProfileCatalogInheritance.js
+++ src/components/OSCALProfileCatalogInheritance.js
@@ -1,4 +1,4 @@
-import React, {useEffect, useState } from 'react';
+import React from 'react';
 import { makeStyles } from "@material-ui/core/styles";
 import { TreeView, TreeItem } from "@material-ui/lab";
 import ExpandMoreIcon from '@material-ui/icons/ExpandMore';
