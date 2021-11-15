8aadadd806ffb4f78872e1bbacc742b589567943
Michael Eisenberg <meisenberg@easydynamics.com>
2021-11-15 11:47:22 -0500

    Resolve linting issues so linter won't fail

--- src/components/OSCALProfileCatalogInheritance.js
+++ src/components/OSCALProfileCatalogInheritance.js
@@ -1,47 +1,53 @@
-import React from 'react';
+import React from "react";
 import { makeStyles } from "@material-ui/core/styles";
 import { TreeView, TreeItem } from "@material-ui/lab";
-import ChevronRightIcon from '@material-ui/icons/ChevronRight';
-import ExpandLessIcon from '@material-ui/icons/ExpandLess';
-import { Grid, IconButton, List, Paper } from '@material-ui/core';
+import ChevronRightIcon from "@material-ui/icons/ChevronRight";
+import ExpandLessIcon from "@material-ui/icons/ExpandLess";
+import { Grid, IconButton, List, Paper } from "@material-ui/core";
 import ListSubheader from "@material-ui/core/ListSubheader";
 
 const useStyles = makeStyles((theme) => ({
-  inheritance : {
+  inheritance: {
     marginTop: theme.spacing(2),
-    marginBottom: theme.spacing(2)
-  }
+    marginBottom: theme.spacing(2),
+  },
 }));
 
 function renderTree(nodes, id) {
-
-  if (nodes["inherited"] == null) {
+  if (nodes.inherited == null) {
     return null;
   }
 
-  const generateLabel = (title, type) => {
-    return (
-      type == "profile" ? "Profile: " + title : "Catalog: " + title
-    );
-  }
+  const generateLabel = (title, type) =>
+    type == "profile" ? `Profile: ${title}` : `Catalog: ${title}`;
 
-  let children = [];
+  const children = [];
 
-  nodes["inherited"].forEach((node) => {
+  nodes.inherited.forEach((node) => {
     id[0] += 1;
-    children.push(<TreeItem
-      nodeId={id[0]} 
-      label={generateLabel(node["title"], node["type"])}
-      children={renderTree(node,id)}
-      expandIcon={<IconButton aria-label={"expand-profiles-and-catalogs " + (id[0] - 1)}>
-        <ChevronRightIcon/>
-      </IconButton>}
-      collapseIcon={<IconButton aria-label={"collapse-profiles-and-catalogs " + (id[0] - 1)}>
-        <ExpandLessIcon/>
-      </IconButton>}>
-    </TreeItem>)
-  })
-  
+    children.push(
+      <TreeItem
+        nodeId={id[0]}
+        label={generateLabel(node.title, node.type)}
+        children={}
+        expandIcon={
+          <IconButton aria-label={`expand-profiles-and-catalogs ${id[0] - 1}`}>
+            <ChevronRightIcon />
+          </IconButton>
+        }
+        collapseIcon={
+          <IconButton
+            aria-label={`collapse-profiles-and-catalogs ${id[0] - 1}`}
+          >
+            <ExpandLessIcon />
+          </IconButton>
+        }
+      >
+        {renderTree(node, id)}
+      </TreeItem>
+    );
+  });
+
   return children;
 }
 
@@ -49,26 +55,27 @@ export default function OSCALProfileCatalogInheritance(props) {
   const classes = useStyles();
   const id = [0];
 
-  return(props.inheritedProfilesAndCatalogs ?
+  return props.inheritedProfilesAndCatalogs ? (
     <Grid item className={classes.inheritance}>
       <Paper>
-        <List subheader={
-              <ListSubheader
-                className={classes.OSCALMetadataPartiesHeader}
-                component="div"
-                id="oscal-metadata-parties"
-              >
-                Profiles/Catalog Inheritance
-              </ListSubheader>
-            }>
+        <List
+          subheader={
+            <ListSubheader
+              className={classes.OSCALMetadataPartiesHeader}
+              component="div"
+              id="oscal-metadata-parties"
+            >
+              Profiles/Catalog Inheritance
+            </ListSubheader>
+          }
+        >
           <TreeView
             aria-label="profile and catalog inheritance display"
-            children={renderTree(props.inheritedProfilesAndCatalogs, id)}
           >
+            {renderTree(props.inheritedProfilesAndCatalogs, id)}
           </TreeView>
         </List>
       </Paper>
     </Grid>
-    : null
-  );
+  ) : null;
 }
--- src/components/OSCALProfileCatalogInheritance.test.js
+++ src/components/OSCALProfileCatalogInheritance.test.js
@@ -1,19 +1,26 @@
 import React from "react";
-import { fireEvent, render, screen, waitFor, within } from "@testing-library/react";
 import {
-  profileCatalogInheritanceData,
-} from "../test-data/CommonData";
+  fireEvent,
+  render,
+  screen,
+  waitFor,
+} from "@testing-library/react";
+import { profileCatalogInheritanceData } from "../test-data/CommonData";
 import OSCALProfileCatalogInheritance from "./OSCALProfileCatalogInheritance";
 
 function profileCatalogInheritanceRenderer() {
-  render(<OSCALProfileCatalogInheritance inheritedProfilesAndCatalogs={profileCatalogInheritanceData}/>);
+  render(
+    <OSCALProfileCatalogInheritance
+      inheritedProfilesAndCatalogs={profileCatalogInheritanceData}
+    />
+  );
 }
 
 test("OSCALProfileCatalogInheritance displays top-level inherited objects", async () => {
   profileCatalogInheritanceRenderer();
   const result = await waitFor(
     () => screen.getByText("Catalog: Example Catalog"),
-    {timeout: 5000}
+    { timeout: 5000 }
   );
   expect(result).toBeVisible();
 });
@@ -26,5 +33,7 @@ test("OSCALProfileCatalogInheritance displays nested inherited objects", async (
     { timeout: 5000 }
   );
   fireEvent.click(inheritanceButton);
-  expect(await screen.findByText("Catalog: Nested Inherited Catalog")).toBeVisible();
+  expect(
+    await screen.findByText("Catalog: Nested Inherited Catalog")
+  ).toBeVisible();
 });
