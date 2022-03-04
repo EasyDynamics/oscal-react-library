c83b3730b74ace24d37839c62055c2938b590c82
Kyle Laker <klaker@easydynamics.com>
2022-03-04 12:36:07 -0500

    Fix typo in docs
    Co-authored-by: Ray Gauss II <rgauss@easydynamics.com>

--- example/src/App.js
+++ example/src/App.js
@@ -63,7 +63,7 @@ function App() {
 
   const [anchorEl, setAnchorEl] = useState(null);
   const [isRestMode, setIsRestMode] = useState(
-    // We want to ensure that throughout the app is is always a boolean
+    // We want to ensure that throughout the app this is always a boolean
     // so that it can be decoupled from the _actual_ API URL (which may
     // be different).
     !!process.env.REACT_APP_REST_BASE_URL
