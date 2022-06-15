ae7cfd4ffd55e2bda7722c7f63a32da110b9aab5
nuttercd <colenutter@gmail.com>
2022-06-15 12:10:10 -0400

    Pass field name as label

--- src/components/OSCALEditableTextField.js
+++ src/components/OSCALEditableTextField.js
@@ -17,7 +17,7 @@ function textFieldWithEditableActions(
         <Grid item xs={props.size} className={props.className}>
           <Typography>
             <TextField
-              aria-label="Text"
+              label={props.fieldName}
               fullWidth
               inputProps={{
                 "data-testid": `textField-${getElementLabel(
