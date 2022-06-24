1b6d4ce29a8d8d9fadcafaed68efbed35624ec51
tuckerzp <ztucker@easydynamics.com>
2022-06-24 08:35:26 -0400

    Create OSCALDrawerSelector files

---
+++ src/components/OSCALDrawerSelector.test.js
@@ -0,0 +1,8 @@
+import { render } from "@testing-library/react";
+import OSCALDrawerSelector from "./OSCALDrawerSelector";
+
+test("Test Drawer even loads", () =>{
+    render(
+        <OSCALDrawerSelector/>
+    )
+});
