a4b8442b233e32e4c0d0d042bc3e0947a2003b4a
kyla <kperkinson@easydynamics.com>
2022-07-01 15:09:26 -0400

    Test for OSCAL object types in drawer

--- src/components/OSCALDrawerSelector.test.js
+++ src/components/OSCALDrawerSelector.test.js
@@ -1,7 +1,42 @@
 import React from "react";
-import { render } from "@testing-library/react";
+import { render, screen } from "@testing-library/react";
 import OSCALDrawerSelector from "./OSCALDrawerSelector";
 
-test("Test Drawer even loads", () => {
-  render(<OSCALDrawerSelector />);
+test("OSCALDrawerSelector loads", () => {
+  render(<OSCALDrawerSelector open />);
+});
+
+test(`OSCALDrawerSelector displays expected tree items`, () => {
+  render(<OSCALDrawerSelector open />);
+
+  const catalogItem = screen.getByRole("treeitem", {
+    name: "Catalog",
+  });
+
+  const componentItem = screen.getByRole("treeitem", {
+    name: "Component",
+  });
+
+  const profileItem = screen.getByRole("treeitem", {
+    name: "Profile",
+  });
+
+  const SSPItem = screen.getByRole("treeitem", {
+    name: "SSP",
+  });
+
+  expect(catalogItem).toBeVisible();
+  expect(componentItem).toBeVisible();
+  expect(profileItem).toBeVisible();
+  expect(SSPItem).toBeVisible();
+});
+
+test(`OSCALDrawerSelector displays close button`, () => {
+  render(<OSCALDrawerSelector open />);
+
+  const closeButton = screen.getByTestId("ChevronLeftIcon", {
+    role: "button",
+  });
+
+  expect(closeButton).toBeVisible();
 });
