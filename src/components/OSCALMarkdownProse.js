d4d08ab9848694674650fc7f8f4a0c92c232b7bc
kperk103 <kperkinson@easydynamics.com>
2022-08-04 11:48:19 -0400

    Add a description for OSCALMarkdownProse and configuration
    This adds a brief description of the OSCALMarkdownProse component
    and adds a configuration in package.json so that the component tests
    will be able to run

--- src/components/OSCALMarkdownProse.js
+++ src/components/OSCALMarkdownProse.js
@@ -1,6 +1,12 @@
 import React from "react";
 import ReactMarkdown from "react-markdown";
 
-export default function OSCALMarkdownProse(props) {
+/**
+ * Renders a string of markdown to React elements
+ * @param {String} props.text 
+ * @returns a React element from the markdown
+ */
+
+export function OSCALMarkdownProse(props) {
   return <ReactMarkdown>{props.text}</ReactMarkdown>;
 }
