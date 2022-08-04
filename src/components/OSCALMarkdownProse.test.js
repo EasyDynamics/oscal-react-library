acb1100bfec223334b315fd3b95844bec6011d1b
schen2166 <schen@easydynamics.com>
2022-08-04 15:23:52 -0400

    add test for script html tags as markdown and multiline markdown
    Note: line does not allow multiple lines so not sure if this is
    what multiline markdown should be.
    
    I am unable to get embedded HTML tags like
    <h1>
     body
      <h2> header 2 </h2>
         more text
    </h1>
    
    I've also tried markdown with mutiple lines sich as
    `const multiline string = "line 1 \
    line 2 \
    line 3\"`
    
    and converting that to HTML and caused linting errors

--- src/components/OSCALMarkdownProse.test.js
+++ src/components/OSCALMarkdownProse.test.js
@@ -2,7 +2,7 @@ import React from "react";
 import ReactDomServer from "react-dom/server";
 import OSCALMarkdownProse from "./OSCALMarkdownProse";
 
-export default function asHtml(input) {
+function asHtml(input) {
   return ReactDomServer.renderToStaticMarkup(input);
 }
 
@@ -42,4 +42,24 @@ test(`OSCALMarkdownProse converts images to HTML`, () => {
     <OSCALMarkdownProse text="`Inserting Tester Code`" />
   );
   expect(testerString).toEqual("<p><code>Inserting Tester Code</code></p>");
+});
+
+test(`OSCALMarkdownProse converts script to HTML`, () => {
+  const testerString = asHtml(
+    <OSCALMarkdownProse text="<script>alert('Nasty script!')</script>" />
+  );
+
+  expect(testerString).toEqual(
+    "&lt;script&gt;alert(&#x27;Nasty script!&#x27;)&lt;/script&gt;"
+  );
+});
+
+test(`OSCALMarkdownProse converts a multiline string to HTML`, () => {
+  const multilineMarkdown =
+    "this should b here # h1 Heading 8-    ## h2 Heading     This is in heading 2    ### h3 Heading      This is in heading 3    #### h4 Heading      ##### **h5 Heading**    ###### h6 Heading ";
+  const multilineAsHTML =
+    "<p>this should b here # h1 Heading 8-    ## h2 Heading     This is in heading 2    ### h3 Heading      This is in heading 3    #### h4 Heading      ##### <strong>h5 Heading</strong>    ###### h6 Heading</p>";
+  const testerString = asHtml(<OSCALMarkdownProse text={multilineMarkdown} />);
+
+  expect(testerString).toEqual(multilineAsHTML);
 });
