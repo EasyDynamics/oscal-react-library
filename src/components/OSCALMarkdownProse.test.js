d47a7fdeffaf58b441fbbc2e4b558d1685c94202
schen2166 <schen@easydynamics.com>
2022-08-04 14:42:17 -0400

    Fix linting errors on Prose Test

--- src/components/OSCALMarkdownProse.test.js
+++ src/components/OSCALMarkdownProse.test.js
@@ -1,19 +1,19 @@
 import React from "react";
-import { OSCALMarkdownProse } from "./OSCALMarkdownProse";
 import ReactDomServer from "react-dom/server";
+import OSCALMarkdownProse from "./OSCALMarkdownProse";
 
-function asHtml(input) {
+export default function asHtml(input) {
   return ReactDomServer.renderToStaticMarkup(input);
 }
 
 test(`OSCALMarkdownProse converts plaintext to HTML`, () => {
-  var testerString = asHtml(<OSCALMarkdownProse text={"Test"} />);
+  const testerString = asHtml(<OSCALMarkdownProse text="Test" />);
   expect(testerString).toEqual("<p>Test</p>");
 });
 
 test(`OSCALMarkdownProse converts Italics, Bold and Plaintext`, () => {
-  var testerString = asHtml(
-    <OSCALMarkdownProse text={"*Hello*, **world** I am in markdown"} />
+  const testerString = asHtml(
+    <OSCALMarkdownProse text="*Hello*, **world** I am in markdown" />
   );
   expect(testerString).toEqual(
     "<p><em>Hello</em>, <strong>world</strong> I am in markdown</p>"
@@ -21,20 +21,25 @@ test(`OSCALMarkdownProse converts Italics, Bold and Plaintext`, () => {
 });
 
 test(`OSCALMarkdownProse converts hyperlinks to HTML`, () => {
-  var testerString = asHtml(
-    <OSCALMarkdownProse
-      text={"[Link to EasyDynamics](https://www.easydynamics.com)]"}
-    />
+  const testerString = asHtml(
+    <OSCALMarkdownProse text="[Link to EasyDynamics](https://www.easydynamics.com)]" />
   );
   expect(testerString).toEqual(
     '<p><a href="https://www.easydynamics.com">Link to EasyDynamics</a>]</p>'
   );
 });
 test(`OSCALMarkdownProse converts images to HTML`, () => {
-  var testerString = asHtml(
+  const testerString = asHtml(
     <OSCALMarkdownProse text={'![alt text](url "title text")'} />
   );
   expect(testerString).toEqual(
     '<p><img src="url" alt="alt text" title="title text"/></p>'
   );
+});
+
+test(`OSCALMarkdownProse converts images to HTML`, () => {
+  const testerString = asHtml(
+    <OSCALMarkdownProse text="`Inserting Tester Code`" />
+  );
+  expect(testerString).toEqual("<p><code>Inserting Tester Code</code></p>");
 });
