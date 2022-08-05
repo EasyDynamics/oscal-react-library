9a1e21d6f69695cfd5201fc5ee8faa170afa24a0
Bronstrom <bfellstrom@easydynamics.com>
2022-08-05 11:35:11 -0400

    Add decimal statement test data & ImplReq test

--- src/test-data/ControlsData.js
+++ src/test-data/ControlsData.js
@@ -51,6 +51,42 @@ export const exampleControl = {
   ],
 };
 
+export const exampleControlWithDecSmt = {
+  id: "control-1",
+  title: "Control 1 Title",
+  params: [
+    {
+      id: "control-1.1_prm_1",
+      label: "control 1.1 / parameter 1 label",
+    },
+    {
+      id: "control-1.1_prm_2",
+      label: "control 1.1 / parameter 2 label",
+    },
+  ],
+  parts: [
+    {
+      id: "control-1.1_smt",
+      name: "statement",
+      prose: "Some organizational group:",
+      parts: [
+        {
+          id: "control-1.1_smt.a",
+          name: "item",
+          props: [
+            {
+              name: "label",
+              value: "a.",
+            },
+          ],
+          prose:
+            "Does something with {{ insert: param, control-1.1_prm_1 }} and {{ insert: param, control-1.1_prm_2 }}",
+        },
+      ],
+    },
+  ],
+};
+
 export const controlsData = [
   {
     id: exampleControl.id,
@@ -142,6 +178,40 @@ export const controlImplTestData = {
                 {
                   "param-id": "control-1_prm_2",
                   values: ["control 1 / component 1 / parameter 2 value"],
+                },
+              ],
+            },
+          ],
+        },
+      ],
+    },
+  ],
+};
+
+export const controlImplWithDecSmtTestData = {
+  description: "This is the control implementation for the system.",
+  "implemented-requirements": [
+    {
+      uuid: "implemented-requirements-1",
+      "control-id": "control-1",
+      statements: [
+        {
+          "statement-id": "control-1.1_smt.a",
+          uuid: "f3887a91-9ed3-425c-b305-21e4634a1c34",
+          "by-components": [
+            {
+              "component-uuid": "component-1.1",
+              uuid: "a74681b2-fbcb-46eb-90fd-0d55aa74ac7b",
+              description:
+                "Component 1.1 description of implementing control 1",
+              "set-parameters": [
+                {
+                  "param-id": "control-1.1_prm_1",
+                  values: ["control 1.1 / component 1.1 / parameter 1 value"],
+                },
+                {
+                  "param-id": "control-1.1_prm_2",
+                  values: ["control 1.1 / component 1.1 / parameter 2 value"],
                 },
               ],
             },
