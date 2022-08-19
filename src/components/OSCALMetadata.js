3b06a1a3af7dc061c95dd6a533b05b6f65214633
tuckerzp <ztucker@easydynamics.com>
2022-08-19 14:58:03 -0400

    Display metadata party info with Dialog modal

--- src/components/OSCALMetadata.js
+++ src/components/OSCALMetadata.js
@@ -3,13 +3,26 @@ import { styled } from "@mui/material/styles";
 import Grid from "@mui/material/Grid";
 import Paper from "@mui/material/Paper";
 import Typography from "@mui/material/Typography";
+import DialogActions from "@mui/material/DialogActions";
+import DialogContent from "@mui/material/DialogContent";
+import DialogTitle from "@mui/material/DialogTitle";
 import Link from "@mui/material/Link";
 import ListSubheader from "@mui/material/ListSubheader";
 import Card from "@mui/material/Card";
+import MapIcon from "@mui/icons-material/Map";
+import AlternateEmailIcon from "@mui/icons-material/AlternateEmail";
+import List from "@mui/material/List";
+import ListItem from "@mui/material/ListItem";
 import CardContent from "@mui/material/CardContent";
 import Avatar from "@mui/material/Avatar";
-import Container from "@mui/material/Container";
 import GroupIcon from "@mui/icons-material/Group";
+import PhoneIcon from "@mui/icons-material/Phone";
+import CardActions from "@mui/material/CardActions";
+import Button from "@mui/material/Button";
+import HomeIcon from "@mui/icons-material/Home";
+import BusinessIcon from "@mui/icons-material/Business";
+import SmartphoneIcon from "@mui/icons-material/Smartphone";
+import Dialog from "@mui/material/Dialog";
 import OSCALEditableTextField from "./OSCALEditableTextField";
 
 export const OSCALMetadataPartiesHeader = styled(ListSubheader)(
@@ -17,14 +30,11 @@ export const OSCALMetadataPartiesHeader = styled(ListSubheader)(
   background-color: ${theme.palette.background.paper};
 `
 );
-const OSCALMetadataLabel = styled(Typography)`
-  text-align: right;
-  color: #0000008a;
-`;
 
-const OSCALMetadataPartyInformation = styled(Container)`
-  color: #0000008a;
-`;
+const OSCALMetadataLabel = styled(Typography)(({ theme }) => ({
+  textAlign: "right",
+  color: theme.palette.text.secondary,
+}));
 
 const OSCALMetadataTitle = styled(Grid)`
   height: 56px;
@@ -49,16 +59,18 @@ const OSCALMetadataPartiesCard = styled(Paper)(({ theme }) => ({
 const formatDate = (isoDate) => new Date(isoDate).toLocaleString();
 
 export function OSCALMetadataPartyAddress(props) {
-  // TODO: Show an icon for the address type (home, work)
   const { address } = props;
   return (
-    <address>
-      <Typography>
-        {`${address["addr-lines"]?.join(", ")} ${address.city} ${
-          address["postal-code"]
-        } ${address.country}`}
-      </Typography>
-    </address>
+    <>
+      {address.type === "home" ? <HomeIcon /> : <BusinessIcon />}
+      <address>
+        <Typography>
+          {`${address["addr-lines"]?.join(", ")} ${address.city} ${
+            address["postal-code"]
+          } ${address.country}`}
+        </Typography>
+      </address>
+    </>
   );
 }
 
@@ -71,13 +83,108 @@ export function OSCALMetadataPartyEmail(props) {
 }
 
 export function OSCALMetadataPartyTelephone(props) {
-  // TODO: Show an icon for the telephone type (home, work, mobile)
+  const { telephone } = props;
+  let icon = null;
+
+  switch (telephone.type) {
+    case "home":
+      icon = <HomeIcon />;
+      break;
+    case "mobile":
+      icon = <SmartphoneIcon />;
+      break;
+    case "office":
+      icon = <BusinessIcon />;
+      break;
+    default:
+      break;
+  }
+
   return (
     <Typography>
-      <Link href={`tel:${props.telephone.number}`}>
-        {props.telephone.number}
-      </Link>
+      {icon}
+      <Link href={`tel:${telephone.number}`}>{telephone.number}</Link>
     </Typography>
+  );
+}
+
+export function OSCALMetadataPartyDialog(props) {
+  const [open, setOpen] = React.useState(false);
+
+  const handleOpen = () => {
+    setOpen(true);
+  };
+
+  const handleClose = () => {
+    setOpen(false);
+  };
+
+  return (
+    <CardActions>
+      <Button size="small" onClick={handleOpen}>
+        Contact
+      </Button>
+      <Dialog
+        open={open}
+        onClose={handleClose}
+        scroll="paper"
+        aria-labelledby="scroll-dialog-title"
+        aria-describedby="scroll-dialog-description"
+        maxWidth="md"
+        fullWidth
+      >
+        <DialogTitle id="scroll-dialog-title">{props.party.name}</DialogTitle>
+        <DialogContent>
+          <Grid container spacing={1}>
+            <Grid item xs={4}>
+              <Typography>
+                <MapIcon />
+                Addresses
+              </Typography>
+              <List>
+                {props.party.addresses?.map((address) => (
+                  <ListItem key={`${address.type}`}>
+                    <OSCALMetadataPartyAddress address={address} />
+                  </ListItem>
+                ))}
+              </List>
+            </Grid>
+
+            <Grid item xs={4}>
+              <Typography>
+                <PhoneIcon />
+                Phone
+              </Typography>
+              <List>
+                {props.party["telephone-numbers"]?.map((telephone) => (
+                  <ListItem key={`${telephone.type}-${telephone.number}`}>
+                    <OSCALMetadataPartyTelephone telephone={telephone} />
+                  </ListItem>
+                ))}
+              </List>
+            </Grid>
+            <Grid item xs={4}>
+              <Typography>
+                <AlternateEmailIcon />
+                Email
+              </Typography>
+              <List>
+                {props.party["email-addresses"]?.map((email) => (
+                  <ListItem key={email}>
+                    <OSCALMetadataPartyEmail email={email} />
+                  </ListItem>
+                ))}
+              </List>
+            </Grid>
+          </Grid>
+        </DialogContent>
+        <DialogActions>
+          <Button onClick={handleClose} color="primary">
+            Close
+          </Button>
+        </DialogActions>
+      </Dialog>
+    </CardActions>
   );
 }
 
@@ -98,24 +205,8 @@ export function OSCALMetadataParty(props) {
             </Typography>
           </Grid>
         </Grid>
-        <OSCALMetadataPartyInformation>
-          {props.party["telephone-numbers"]?.map((telephone) => (
-            <OSCALMetadataPartyTelephone
-              key={`${telephone.type}-${telephone.number}`}
-              telephone={telephone}
-            />
-          ))}
-          {props.party["email-addresses"]?.map((email) => (
-            <OSCALMetadataPartyEmail key={email} email={email} />
-          ))}
-          {props.party.addresses?.map((address) => (
-            <OSCALMetadataPartyAddress
-              key={`${address.type}`}
-              address={address}
-            />
-          ))}
-        </OSCALMetadataPartyInformation>
       </CardContent>
+      <OSCALMetadataPartyDialog party={props.party} />
     </Card>
   );
 }
