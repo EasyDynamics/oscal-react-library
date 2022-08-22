20d1f179c58514d69226f318b91f461d1b616f67
tuckerzp <ztucker@easydynamics.com>
2022-08-22 10:34:02 -0400

    Test Metadata Contact button
    Rather than having the parent components of OSCALMetadata test
    functionality, the tests have been rewritten to be more clear on exactly
    what is being tested.

--- src/components/OSCALMetadata.test.js
+++ src/components/OSCALMetadata.test.js
@@ -1,33 +1,78 @@
 import React from "react";
-import { render, screen, within } from "@testing-library/react";
+import { render, screen } from "@testing-library/react";
 import OSCALMetadata from "./OSCALMetadata";
 import { metadataTestData } from "../test-data/CommonData";
 
-function metadataRenderer() {
-  render(<OSCALMetadata metadata={metadataTestData} />);
-}
+describe("OSCALMetadata", () => {
+  beforeEach(() => {
+    render(<OSCALMetadata metadata={metadataTestData} />);
+  });
 
-export default function testOSCALMetadata(parentElementName, renderer) {
-  test(`${parentElementName} displays title`, () => {
-    renderer();
+  test(`displays title`, () => {
     const result = screen.getByRole("heading", { name: "Test Title" });
     expect(result).toBeVisible();
   });
 
-  test(`${parentElementName} displays version`, () => {
-    renderer();
+  test(`displays version`, () => {
     const result = screen.getByText("Revision 5");
     expect(result).toBeVisible();
   });
 
-  test(`${parentElementName} displays parties`, () => {
-    renderer();
-    const partiesElement = screen.getByText("Parties").closest("ul");
-    const result = within(partiesElement).getByText("Some group of people");
+  test(`displays parties`, () => {
+    const result = screen.getByText("Some group of people");
     expect(result).toBeVisible();
   });
-}
 
-if (!require.main) {
-  testOSCALMetadata("OSCALMetadata", metadataRenderer);
-}
+  test(`displays Contact button`, () => {
+    const button = screen.getByRole("button", {
+      name: /contact/i,
+    });
+
+    expect(button).toBeVisible();
+  });
+
+  test(`displays email contact info`, () => {
+    const button = screen.getByRole("button", {
+      name: /contact/i,
+    });
+
+    button.click();
+
+    expect(screen.getByText("Email")).toBeVisible();
+    expect(
+      screen.getByRole("link", { name: /owners@email\.org/i })
+    ).toBeVisible();
+  });
+
+  test(`displays telephone mobile number info`, () => {
+    const button = screen.getByRole("button", {
+      name: /contact/i,
+    });
+    button.click();
+
+    expect(screen.getByText("Phone")).toBeVisible();
+    expect(screen.getByRole("link", { name: /\+18005555555/i })).toBeVisible();
+  });
+
+  test(`displays telephone office number info`, () => {
+    const button = screen.getByRole("button", {
+      name: /contact/i,
+    });
+
+    button.click();
+
+    expect(screen.getByText("Phone")).toBeVisible();
+    expect(screen.getByRole("link", { name: /\+18004444444/i })).toBeVisible();
+  });
+
+  test(`displays address info`, () => {
+    const button = screen.getByRole("button", {
+      name: /contact/i,
+    });
+
+    button.click();
+
+    expect(screen.getByText("Addresses")).toBeVisible();
+    expect(screen.getByText(/0000 st, suite 3 city 0000 us/i)).toBeVisible();
+  });
+});
--- src/test-data/CommonData.js
+++ src/test-data/CommonData.js
@@ -3,6 +3,27 @@ export const exampleParties = [
     uuid: "party-1",
     type: "organization",
     name: "Some group of people",
+    "email-addresses": ["owners@email.org"],
+    "telephone-numbers": [
+      {
+        type: "mobile",
+        number: "+18005555555",
+      },
+      {
+        type: "office",
+        number: "+18004444444",
+      },
+    ],
+    addresses: [
+      {
+        type: "work",
+        "addr-lines": ["0000 St", "Suite 3"],
+        city: "City",
+        state: "State",
+        "postal-code": "0000",
+        country: "US",
+      },
+    ],
   },
 ];
 
