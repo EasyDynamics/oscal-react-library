8d4a1f327a11a1d5cea7f782786ac2958fbb8ced
nuttercd <colenutter@gmail.com>
2022-09-12 10:55:37 -0400

    Update test description

--- src/components/OSCALControlGuidance.test.js
+++ src/components/OSCALControlGuidance.test.js
@@ -3,12 +3,14 @@ import { render, screen } from "@testing-library/react";
 import userEvent from "@testing-library/user-event";
 import OSCALControlGuidance from "./OSCALControlGuidance";
 
-test("OSCALCatalog displays control guidance", async () => {
-  const prose = "Access control policy";
-  const id = "AC-1";
-  const title = "Sample Title";
-  render(<OSCALControlGuidance prose={prose} id={id} title={title} />);
-  await userEvent.click(screen.getByRole("button"));
-  const result = screen.getByText("Access control policy");
-  expect(result).toBeVisible();
+describe("OSCALControlGuidance", () => {
+  test("displays guidance modal", async () => {
+    const prose = "Access control policy";
+    const id = "AC-1";
+    const title = "Sample Title";
+    render(<OSCALControlGuidance prose={prose} id={id} title={title} />);
+    await userEvent.click(screen.getByRole("button"));
+    const result = screen.getByText("Access control policy");
+    expect(result).toBeVisible();
+  });
 });
