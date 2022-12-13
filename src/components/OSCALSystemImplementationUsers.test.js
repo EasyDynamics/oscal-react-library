71333aac5d2a4eee15ccb4ab4be9a66315f38d14
Kyle Laker <klaker@easydynamics.com>
2022-12-12 19:31:40 -0500

    Remove `render` calls from `beforeEach`
    These violate lint rule `testing-library/no-render-in-setup`. This
    results in more duplication but with that, we get more clarity in tests
    and flexibility.

--- src/components/OSCALSystemImplementationUsers.test.js
+++ src/components/OSCALSystemImplementationUsers.test.js
@@ -4,46 +4,50 @@ import { usersTestData } from "../test-data/SystemData";
 import OSCALSystemImplementationUsers from "./OSCALSystemImplementationUsers";
 
 describe("OSCALSystemImplementationUsers", () => {
-  beforeEach(() => {
-    render(<OSCALSystemImplementationUsers users={usersTestData} />);
-  });
-
   test("shows 'Users' section title", () => {
+    render(<OSCALSystemImplementationUsers users={usersTestData} />);
     const result = screen.getByText("Users");
     expect(result).toBeVisible();
   });
 
   test("shows 'Title' column", () => {
+    render(<OSCALSystemImplementationUsers users={usersTestData} />);
     const result = screen.getByText("Title");
     expect(result).toBeVisible();
   });
 
   test("shows 'Authorized Privileges' column", () => {
+    render(<OSCALSystemImplementationUsers users={usersTestData} />);
     const result = screen.getByText("Authorized Privileges");
     expect(result).toBeVisible();
   });
 
   test("shows name of a user listed", () => {
+    render(<OSCALSystemImplementationUsers users={usersTestData} />);
     const result = screen.getByText("User 1");
     expect(result).toBeVisible();
   });
 
   test("shows name of a privilege listed", () => {
+    render(<OSCALSystemImplementationUsers users={usersTestData} />);
     const result = screen.getByText("privilege title");
     expect(result).toBeVisible();
   });
 
   test("shows name of a user listed", async () => {
+    render(<OSCALSystemImplementationUsers users={usersTestData} />);
     await userEvent.hover(screen.getByText("User 1"));
     expect(await screen.findByText("A system user")).toBeInTheDocument();
   });
 
   test("shows 'Authorized Privileges' title", () => {
+    render(<OSCALSystemImplementationUsers users={usersTestData} />);
     const result = screen.getByText("privilege title");
     expect(result).toBeVisible();
   });
 
   test("shows 'Authorized Privileges' description", async () => {
+    render(<OSCALSystemImplementationUsers users={usersTestData} />);
     await userEvent.hover(screen.getByText("privilege title"));
     expect(
       await screen.findByText("privilege description")
@@ -51,11 +55,13 @@ describe("OSCALSystemImplementationUsers", () => {
   });
 
   test("shows read function preformed", () => {
+    render(<OSCALSystemImplementationUsers users={usersTestData} />);
     const result = screen.getByText("reading function");
     expect(result).toBeVisible();
   });
 
   test("shows write function preformed", () => {
+    render(<OSCALSystemImplementationUsers users={usersTestData} />);
     const result = screen.getByText("writing function");
     expect(result).toBeVisible();
   });
