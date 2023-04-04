7b4ca264852c695b107fb2c6d81a4a27157cda01
Zachary Tucker <ztucker@easydynamics.com>
2023-04-03 21:12:08 -0400

    feat(backmatter): Support viewing base64 data (#751)
    This reworks how we resolve links in backmatter to not always assume
    data will be in an `rlink`.
    
    The `BackMatterLookup` class will make resolving back matter a lot
    easier in the future.
    
    ### Testing
    
    
    http://localhost:3000/system-security-plan/?url=https://raw.githubusercontent.com/EasyDynamics/oscal-demo-content/tuckerzp/base64/system-security-plans/ssp-example.json
    
    The dataflow diagram is in base64
    
    
    ![image](https://user-images.githubusercontent.com/39490074/229220843-945580e5-1e13-4428-8cea-8dc4c86e3622.png)
    
    closes #671

--- src/components/oscal-utils/OSCALBackMatterUtils.js
+++ src/components/oscal-utils/OSCALBackMatterUtils.js
@@ -1,41 +1,79 @@
-/**
- * Gets a resource from the given back matter's resource by the given HREF UUID.
- */
-export function getResourceFromBackMatterByHref(backMatter, href) {
-  if (!href.startsWith("#")) {
-    throw new Error("not an internal href");
-  }
-  // Dig into back-matter to look for absolute href
-  const importUuid = href.substring(1);
+export default class BackMatterLookup {
+  #backMatter;
 
-  return backMatter?.resources?.find(
-    (resource) => resource.uuid === importUuid
-  );
-}
+  #preferBase64 = false;
 
-/**
- * Gets an rlink URI from the given back matter's resource by the given HREF UUID.
- *
- * @see {@link https://pages.nist.gov/OSCAL/documentation/schema/implementation-layer/component/xml-schema/##local_back-matter-resource-rlink_def-h3}
- */
-export default function getUriFromBackMatterByHref(
-  backMatter,
-  href,
-  mediaTypeRegex
-) {
-  const foundResource = getResourceFromBackMatterByHref(backMatter, href);
-
-  if (!foundResource) {
-    throw new Error("resource not found for href");
+  // the `preferBase64` parameter would allow operating "offline" without access to
+  // public resources
+  constructor(backMatter, preferBase64) {
+    this.#backMatter = backMatter;
+    this.#preferBase64 = preferBase64;
   }
 
-  const foundLink = foundResource.rlinks.find((rlink) =>
-    mediaTypeRegex.test(rlink["media-type"])
-  );
+  /**
+   * @param uuid {string}
+   * @param mediaType {?RegExp}
+   *
+   * @returns ResolvedUri {string} or undefined
+   */
+  resolveUuid(uuid, mediaType) {
+    const resource = this.#backMatter.resources.find(
+      (item) => item.uuid === uuid
+    );
 
-  if (!foundLink) {
-    throw new Error("resource not found for href");
+    if (!this.#preferBase64) {
+      const rlink = resource?.rlinks?.find((item) => {
+        const rlinkMediaType = item["media-type"];
+        if (!mediaType || !rlinkMediaType) {
+          return true;
+        }
+        return mediaType.test(rlinkMediaType);
+      });
+
+      if (rlink) {
+        return { uri: rlink.href, mediaType: rlink["media-type"] };
+      }
+    }
+
+    const base64 = resource?.base64;
+
+    // We require media type to be present. In most cases in existing OSCAL documents,
+    // media type is missing; however, the underlying document is not actually plain-text
+    // data. If we don't specify a media type in the URI, the browser will assume that it
+    // is plain text. This will cause a whole mess that we'd really rather avoid.
+    // In the future, this may throw an error.
+    if (!base64?.["media-type"]) {
+      return undefined;
+    }
+
+    if (mediaType && !mediaType.test(base64["media-type"])) {
+      return undefined;
+    }
+
+    if (!base64?.value) {
+      return undefined;
+    }
+
+    return {
+      uri: `data:${base64["media-type"]};base64,${base64.value.replace(
+        "\n",
+        ""
+      )}`,
+      mediaType: base64["media-type"],
+    };
   }
 
-  return foundLink;
+  /**
+   * @param uri {string}
+   * @param mediaType {?string}
+   *
+   * @returns ResolvedUri {string} or undefined
+   */
+  resolve(uri, mediaType) {
+    if (uri.startsWith("#")) {
+      return this.resolveUuid(uri.slice(1), mediaType);
+    }
+
+    return { uri, mediaType: undefined };
+  }
 }
