16e0bed51cb3b7fe2d89bf7ce52a730e873ae903
Kyle Laker <klaker@easydynamics.com>
2023-04-07 20:42:39 -0400

    Re-export fun names
    https://github.com/quicktype/quicktype/blob/d2e7d4d7b7d5c37ab8db225054a95fe78f182158/packages/quicktype-core/src/Naming.ts#L151-L169

--- packages/oscal-types/src/index.ts
+++ packages/oscal-types/src/index.ts
@@ -1,1 +1,12 @@
 export * from "./oscal";
+
+// Because of the type generation, some items get generated with "fun" names by the
+// `quicktype` library. Rather than modifying the `oscal.ts` file, they are exported
+// using better names.
+// It is possible that in the future, these may flip (or other "State"s will be added
+// causing these to change). We should keep an eye on this. Consumers of the library
+// should only consider the more serious-looking names to be stable.
+export {
+  FluffyState as SystemCharacteristicStatusState,
+  PurpleState as ComponentStatusState,
+} from "./oscal";
