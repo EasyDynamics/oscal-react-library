78aeff7448deac7ff516225e0ca04d3fb57cae1d
Kyle Laker <klaker@easydynamics.com>
2023-04-27 13:26:28 -0400

    feat: display back matter resource types (#793)
    This adds the `type` prop more prominently on back matter resources,
    converting it to a human-friendly name. This refactors the back matter
    cards to use the `CardHeader` so that it's easier to add a subtitle.
    Additionally, cards use 100% of their height so they're all the same
    size, which makes them appear more consistent.
    
    
    ![image](https://user-images.githubusercontent.com/850893/234904653-c0b14247-6e98-4162-ab28-c06e0f538590.png)
    
    Resolves https://github.com/EasyDynamics/oscal-react-library/issues/789

--- packages/oscal-react-library/src/components/OSCALBackMatter.tsx
+++ packages/oscal-react-library/src/components/OSCALBackMatter.tsx
@@ -2,6 +2,7 @@ import React from "react";
 import { styled } from "@mui/material/styles";
 import Card from "@mui/material/Card";
 import CardContent from "@mui/material/CardContent";
+import CardHeader from "@mui/material/CardHeader";
 import Chip from "@mui/material/Chip";
 import Grid from "@mui/material/Grid";
 import Stack from "@mui/material/Stack";
@@ -16,6 +17,7 @@ import { OSCALAnchorLinkHeader } from "./OSCALAnchorLinkHeader";
 import OSCALEditableTextField, { EditableFieldProps } from "./OSCALEditableTextField";
 import { Resource, ResourceLink, BackMatter } from "@easydynamics/oscal-types";
 import { ReactElement } from "react";
+import { propWithName } from "./oscal-utils/OSCALPropUtils";
 
 export const OSCALBackMatterCard = styled(Card)(
   ({ theme }) => `
@@ -24,6 +26,78 @@ export const OSCALBackMatterCard = styled(Card)(
     flex-direction: column;
 `
 );
+
+// This maps the well-known back matter types to more human-readable names
+// based on their descriptions.
+const backMatterTypeLookup: Record<string, string> = {
+  logo: "Logo",
+  image: "Image",
+  "screen-shot": "Screenshot",
+  law: "Law",
+  regulation: "Regulation",
+  standard: "Standard",
+  "external-guidance": "External Guidance",
+  acronymns: "Acronyms",
+  citation: "Citation",
+  policy: "Policy",
+  procedure: "Procedure",
+  "system-guide": "System Guide",
+  "users-guide": "User's/Administrator's Guide",
+  "administrators-guide": "Administrator's Guide",
+  "rules-of-behavior": "Rules of Behavior",
+  plan: "Plan",
+  artifact: "Artifact",
+  evidence: "Evidence",
+  "tool-output": "Tool output",
+  "raw-data": "Raw Data",
+  "interview-notes": "Interview Notes",
+  questionnaire: "Questionnaire",
+  report: "Report",
+  agreement: "Agreement",
+};
+
+interface BackMatterTypeRepresentationOptions {
+  /**
+   * A mapping of possible values for the `type` field to descriptive names.
+   *
+   * @default - `backMatterTypeLookup`
+   */
+  mapToFriendlyName?: Record<string, string>;
+
+  /**
+   * For values not in the given mapping, whether to transform the name from
+   * the typical kebab-case to title case.
+   *
+   * @default - no conversion is applied to unknown values
+   */
+  convertToTitleCase?: boolean;
+}
+
+/**
+ * Transform the text for a `type` property on a back matter resource to a more
+ * human-friendly representation.
+ *
+ * @param value the string to transform
+ * @param opts options for the transformation
+ * @returns the transformed string
+ */
+function backMatterTypeRepresentation(
+  value: string,
+  opts?: BackMatterTypeRepresentationOptions
+): string {
+  const knownName = (opts?.mapToFriendlyName ?? backMatterTypeLookup)[value];
+  if (knownName) {
+    return knownName;
+  }
+
+  if (opts?.convertToTitleCase) {
+    return value
+      .split("-")
+      .map((str) => str.charAt(0).toUpperCase() + str.slice(1))
+      .join(" ");
+  }
+  return value;
+}
 
 interface TitleDisplayProps {
   /**
@@ -106,42 +180,41 @@ function BackMatterResource(props: BackMatterResourceProps) {
     rlink["media-type"] ||
     guessExtensionFromHref(getAbsoluteUrl(rlink.href, props.parentUrl) ?? "");
 
+  const resourceType = propWithName(resource.props, "type")?.value;
+  const typeDisplay = resourceType && backMatterTypeRepresentation(resourceType);
   const objectKey = getObjectRootKey(props.partialRestData);
 
   return (
     <Grid item xs={3} key={resource.uuid}>
-      <OSCALBackMatterCard>
+      <OSCALBackMatterCard sx={{ height: "100%" }}>
+        <CardHeader
+          title={
+            <TitleDisplay uuid={resource.uuid}>
+              <OSCALEditableTextField
+                fieldName="title"
+                isEditable={props.isEditable}
+                editedField={props.isEditable ? [objectKey, "back-matter", "resources"] : null}
+                editedValue={props.backMatter?.resources}
+                editedValueId={resource.uuid}
+                onFieldSave={props.onFieldSave}
+                partialRestData={
+                  props.isEditable
+                    ? {
+                        [objectKey]: {
+                          uuid: props.partialRestData?.[objectKey].uuid,
+                          "back-matter": props.backMatter,
+                        },
+                      }
+                    : null
+                }
+                value={resource.title}
+              />
+            </TitleDisplay>
+          }
+          subheader={typeDisplay}
+          action={<CitationDisplay resource={resource} />}
+        />
         <CardContent>
-          <Grid container spacing={0}>
-            <Grid item xs={11}>
-              <TitleDisplay uuid={resource.uuid}>
-                <OSCALEditableTextField
-                  fieldName="title"
-                  isEditable={props.isEditable}
-                  editedField={props.isEditable ? [objectKey, "back-matter", "resources"] : null}
-                  editedValue={props.backMatter?.resources}
-                  editedValueId={resource.uuid}
-                  onFieldSave={props.onFieldSave}
-                  partialRestData={
-                    props.isEditable
-                      ? {
-                          [objectKey]: {
-                            uuid: props.partialRestData?.[objectKey].uuid,
-                            "back-matter": props.backMatter,
-                          },
-                        }
-                      : null
-                  }
-                  value={resource.title}
-                />
-              </TitleDisplay>
-            </Grid>
-            <Grid item xs={1}>
-              <Grid container spacing={0} justifyContent="flex-end">
-                <CitationDisplay resource={resource} />
-              </Grid>
-            </Grid>
-          </Grid>
           <OSCALEditableTextField
             fieldName="description"
             isEditable={props.isEditable}
