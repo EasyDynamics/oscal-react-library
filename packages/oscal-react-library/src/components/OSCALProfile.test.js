294deb23e649fc15c9ed399bbece715659f041c7
Zachary Tucker <ztucker@easydynamics.com>
2023-05-04 17:34:09 -0400

    fix(tests): Tests time out and fail (#839)

--- packages/oscal-react-library/src/components/OSCALProfile.test.js
+++ packages/oscal-react-library/src/components/OSCALProfile.test.js
@@ -1,41 +1,59 @@
 import React from "react";
 import { render, screen, fireEvent } from "@testing-library/react";
-import { OSCALProfileLoader } from "./OSCALLoader";
 import OSCALProfile from "./OSCALProfile";
 import { parentUrlTestData } from "../test-data/Urls";
 import profileTestData from "../test-data/ProfileData";
 
-test("OSCALProfile loads", () => {
-  render(<OSCALProfileLoader />);
-});
+describe("The OSCAL Profile", () => {
+  it("displays controls", async () => {
+    render(
+      <OSCALProfile
+        profile={profileTestData}
+        parentUrl={parentUrlTestData}
+        onResolutionComplete={() => {}}
+      />
+    );
 
-function profileRenderer() {
-  render(
-    <OSCALProfile
-      profile={profileTestData}
-      parentUrl={parentUrlTestData}
-      onResolutionComplete={() => {}}
-    />
-  );
-}
-
-function testOSCALProfile(parentElementName, renderer) {
-  test(`${parentElementName} displays controls`, async () => {
-    renderer();
     const result = await screen.findByText("AC-1", {
       timeout: 30_000,
     });
-    expect(result).toBeVisible();
-  });
 
-  test(`${parentElementName} displays parameter constraints`, async () => {
-    renderer();
+    expect(result).toBeVisible();
+  }, 30_000);
+
+  it("displays parameter constraints", async () => {
+    render(
+      <OSCALProfile
+        profile={profileTestData}
+        parentUrl={parentUrlTestData}
+        onResolutionComplete={() => {}}
+      />
+    );
+
     const result = await screen.findAllByText("< organization-defined frequency >", {
       timeout: 30_000,
     });
     fireEvent.mouseOver(result[0]);
     expect(await screen.findByText("at least every 3 years")).toBeVisible();
   });
-}
 
-testOSCALProfile("OSCALProfile", profileRenderer);
+  it("displays profile control modifications", async () => {
+    render(
+      <OSCALProfile
+        profile={profileTestData}
+        parentUrl={parentUrlTestData}
+        onResolutionComplete={() => {}}
+      />
+    );
+
+    const modButton = await screen.findByRole(
+      "button",
+      { name: "ac-1 modifications" },
+      { timeout: 10000 }
+    );
+    fireEvent.click(modButton);
+
+    expect(screen.getByText("Modifications")).toBeVisible();
+    expect(screen.getByText("Adds")).toBeVisible();
+  }, 30000);
+});
