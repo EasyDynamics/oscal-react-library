4d3f9609653d044fed611fd59ff31ebc74189a1e
Bradley Fellstrom <35196021+Bronstrom@users.noreply.github.com>
2023-05-05 11:21:34 -0400

    feat: Add properties story (#843)
    This adds a properties story in storybook.
    
    Closes #718

--- packages/oscal-react-library/src/components/OSCALProperties.tsx
+++ packages/oscal-react-library/src/components/OSCALProperties.tsx
@@ -99,7 +99,7 @@ const OSCALProperties: React.FC<OSCALPropertiesProps> = ({ properties, namespace
   );
 };
 
-interface OSCALPropertiesDialogProps {
+export interface OSCALPropertiesDialogProps {
   /**
    * Contains properties elements
    */
