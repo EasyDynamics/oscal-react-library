ffbcceadd9131ce9700a4f4a67b5140f8cf831a2
Moustapha Pemy <mpemy@easydynamics.com>
2023-05-26 09:51:54 -0400

    adjusting the test to reflect the objective

--- packages/oscal-react-library/src/components/OSCALControlPart.test.js
+++ packages/oscal-react-library/src/components/OSCALControlPart.test.js
@@ -1,39 +1,34 @@
 import React from "react";
 import { render, screen, fireEvent } from "@testing-library/react";
 import { OSCALControlPart } from "./OSCALControlPart";
-import {
-  OSCALReplacedProseWithParameterLabel,
-  OSCALReplacedProseWithByComponentParameterValue,
-} from "./OSCALControlProse";
-import {
-  exampleModificationSetParameters,
-  exampleModificationSetParametersDecimal,
-} from "../test-data/ModificationsData";
-import {
-  controlImplTestData,
-  controlImplWithDecSmtTestData,
-  controlProseTestData,
-  controlProseDecimalTestData,
-  exampleParams,
-  exampleDecimalParams,
-} from "../test-data/ControlsData";
-import { sspRestData } from "../test-data/SystemData";
+import { OSCALControlRemovedPartWrapper } from "./OSCALControlProse";
+import { controlImplTestData, controlProseTestData } from "../test-data/ControlsData";
 
-const labelTestData = "label-1";
-const labelTestDataDecimal = "label-1.1";
 
-describe("OSCALControlProse", () => {
-  test("displays parameter labels", async () => {
+describe("OSCALControlPart", () => {
+  test("displays removed part", async () => {
     render(
-      <OSCALReplacedProseWithParameterLabel
-        implementedRequirement={controlImplTestData["implemented-requirements"][0]}
-        label={labelTestData}
-        prose={controlProseTestData}
-        parameters={exampleParams}
-        modificationSetParameters={exampleModificationSetParameters}
-      />
+      <OSCALControlRemovedPartWrapper ownerState partName={controlImplTestData.part.name}>
+        {controlProseTestData}
+        {controlImplTestData.part.parts?.map((part) => (
+          <OSCALControlPart
+            part={part}
+            control={controlImplTestData.control}
+            controlId={controlImplTestData.controlId ?? controlImplTestData.control.id}
+            parameters={controlImplTestData.parameters}
+            implementedRequirement={controlImplTestData.implementedRequirement}
+            componentId={controlImplTestData.componentId}
+            modificationAlters={controlImplTestData.modificationAlters}
+            modificationSetParameters={controlImplTestData.modificationSetParameters}
+            key={part.id ?? part.name}
+            isEditable={controlImplTestData.isEditable}
+            onRestSuccess={controlImplTestData.onRestSuccess}
+            onRestError={controlImplTestData.onRestError}
+            partialRestData={controlImplTestData.partialRestData}
+          />
+        ))}
+      </OSCALControlRemovedPartWrapper>
     );
-
     const result = await screen.findByText("< control 1 label >");
     fireEvent.mouseOver(result);
     expect(await screen.findByText("some constraint")).toBeVisible();
