b7f67518b87c5a95dbb14825a4bb9cac69378730
Bradley Fellstrom <bfellstrom@easydynamics.com>
2023-08-08 11:00:25 -0400

    Resolve viewer linter failures

--- packages/oscal-viewer/src/themes/AppTheme.js
+++ packages/oscal-viewer/src/themes/AppTheme.js
@@ -1,17 +1,67 @@
 import { createTheme } from "@mui/material/styles";
 
-const primaryBlue = "#001131";
+// Common palette colors
+const primaryBlue = "#002867";
 const secondaryBlue = "#023E88";
-// const backgroundGray = "#F6F6F6";
-// const primaryAccent = "#FF6600";
+const backgroundGray = "#F6F6F6";
+const primaryAccent = "#FF6600";
 
 export const appTheme = createTheme({
   palette: {
+    // Primary color palette
     primary: {
       main: primaryBlue,
     },
     secondary: {
       main: secondaryBlue,
+    },
+    backgroundGray: {
+      main: backgroundGray,
+    },
+    primaryAccent: {
+      main: primaryAccent,
+    },
+    // Secondary color palette
+    lightGrayBlue: {
+      main: "#E8EBF1",
+    },
+    offWhite: {
+      main: "#F3F5F8",
+    },
+    white: {
+      main: "#FFFFFF",
+    },
+    black: {
+      main: "#2B2B2B",
+    },
+    gray: {
+      main: "#EAEAEA",
+    },
+    destructive: {
+      main: "#B31515",
+      dark: "#9B1616",
+    },
+    lightPink: {
+      main: "#FFD9D9",
+      dark: "#EEC8C8",
+    },
+    lightGreen: {
+      main: "#DBF4E7",
+      dark: "#CAE3D6",
+    },
+    lightYellow: {
+      main: "#F8ECCD",
+    },
+    lightBlue: {
+      main: "#A3C2EE",
+      dark: "#649DED",
+    },
+    grayBlue: {
+      main: "#B4BCCC",
+    },
+    // Other colors
+    darkGray: {
+      main: "#6A6A6A",
     },
   },
   typography: {
@@ -21,13 +71,25 @@ export const appTheme = createTheme({
     },
     h2: {
       fontWeight: "700",
-      fontSize: "1.375rem",
       color: primaryBlue,
+      fontSize: "1.375rem",
     },
-    body: {
+    h3: {
+      fontWeight: "400",
+      fontSize: "1.25rem",
+    },
+    body1: {
       fontWeight: "400",
       fontSize: "1rem",
+      letterSpacing: ".05rem",
     },
+    body2: {
+      fontSize: "0.85rem",
+    },
+    fontWeightLight: "300",
+    fontWeightRegular: "400",
+    fontWeightMedium: "600",
+    fontWeightBold: "700",
     fontFamily: ["Source Sans Pro", "sans-serif"].join(","),
   },
   components: {
@@ -36,7 +98,7 @@ export const appTheme = createTheme({
         root: {
           fontWeight: "600",
           fontSize: "16px",
-          borderRadius: "5px",
+          borderRadius: "3px",
           lineHeight: "20px",
         },
         contained: {
